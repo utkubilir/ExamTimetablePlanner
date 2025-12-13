33a5ce06ae9b941aae0403a04d010688f8a00c13
utkubilir <149961141+utkubilir@users.noreply.github.com>
2025-12-13 14:49:53 +0300

    create MainApp class for testing application

---
+++ MainApp.java
@@ -0,0 +1,31 @@
+package com.examplanner.ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) {
+        try {
+
+            com.examplanner.persistence.DatabaseManager.initializeDatabase();
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("MainView.fxml"));
+            scene = new Scene(fxmlLoader.load(), 1200, 800);
+            stage.setScene(scene);
+            stage.setTitle("Exam Timetable Planner");
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Failed to start application: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
